7b88ed0f3e27ecc9fe10b905279205b22cef6dcf
Andy Bursavich <abursavich@gmail.com>
2023-03-10 16:51:19 -0800

    Update generated testdata gRPC code

--- testdata/regen.sh
+++ testdata/regen.sh
@@ -1,5 +1,8 @@
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
 protoc --go_out="$DIR" --go_opt=paths=source_relative \
     --go-grpc_out="$DIR" --go-grpc_opt=paths=source_relative \
     --proto_path="$DIR" "$DIR"/*/*.proto
